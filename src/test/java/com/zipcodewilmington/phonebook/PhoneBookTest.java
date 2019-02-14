4456d0e5d3e69790115b0618eaa37debcf1bf237
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-02-14 17:00:36 -0500

    Created a PhoneBook that keeps track of names and ArrayLists of phone numbers as strings. Wrote 22 test casses to test 11 methods.

--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,17 @@
 package com.zipcodewilmington.phonebook;
 
-/**
- * Created by leon on 1/23/18.
- */
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
 public class PhoneBookTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
